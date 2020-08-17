9ca18489089cdc9592b749c3da9a420b4417c4b3
Eric Denman <edenman@users.noreply.github.com>
2020-08-18 09:32:31 +1200

    Demonstrate issue with using nullable and non-nullable serializer for the same type

--- runtime/commonTest/src/kotlinx/serialization/SerializationForNullableTypeOnFileTest.kt
+++ runtime/commonTest/src/kotlinx/serialization/SerializationForNullableTypeOnFileTest.kt
@@ -1,7 +1,7 @@
 /*
  * Copyright 2017-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
-@file:UseSerializers(MyIntSerializer::class)
+@file:UseSerializers(MyNullableIntSerializer::class, MyNonNullableIntSerializer::class)
 
 package kotlinx.serialization
 
@@ -14,25 +14,41 @@ import kotlin.test.*
 class SerializationForNullableTypeOnFileTest {
 
     @Serializable
-    data class Holder(val i: Int?)
+    data class Holder(val nullable: Int?, val nonNullable: Int)
 
     @Serializer(forClass = Int::class)
-    object MyIntSerializer : KSerializer<Int?> {
+    object MyNullableIntSerializer : KSerializer<Int?> {
         override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("MIS", PrimitiveKind.INT).nullable
 
         override fun serialize(encoder: Encoder, value: Int?) {
-            if (value == null) encoder.encodeInt(42)
-            else encoder.encodeInt(239)
+            if (value == null || value == 0) encoder.encodeNull()
+            else encoder.encodeInt(value)
+        }
+
+        override fun deserialize(decoder: Decoder): Int? {
+            val value = decoder.decodeInt()
+            return if (value == 0) null else value
+        }
+    }
+
+    @Serializer(forClass = Int::class)
+    object MyNonNullableIntSerializer : KSerializer<Int> {
+        override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor("MIS", PrimitiveKind.INT)
+
+        override fun serialize(encoder: Encoder, value: Int) {
+            return encoder.encodeInt(value)
         }
 
         override fun deserialize(decoder: Decoder): Int {
-            TODO()
+            return decoder.decodeInt()
         }
     }
 
     @Test
     fun testFileLevel() {
-        assertEquals("""{"i":42}""", Json.encodeToString(Holder(null)))
-        assertEquals("""{"i":239}""", Json.encodeToString(Holder(314)))
+        assertEquals("""{"nullable":null,"nonNullable":51}""", Json.encodeToString(Holder(nullable = null, nonNullable = 51)))
+        assertEquals("""{"nullable":314,"nonNullable":51}""", Json.encodeToString(Holder(nullable = 314, nonNullable = 51)))
+        assertEquals(Holder(nullable = null, nonNullable = 51), Json.decodeFromString("""{"nullable":0,"nonNullable":51}"""))
+        assertEquals(Holder(nullable = 314, nonNullable = 51), Json.decodeFromString("""{"nullable":314,"nonNullable":51}"""))
     }
 }
